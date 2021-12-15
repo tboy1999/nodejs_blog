dc072b3345701f5d679b37c784127b0e4885519b
Tong Tai <meotai1999@gmail.com>
2021-12-15 14:29:55 +0700

    add file route vs coontroller

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+
+class SiteController {
+
+    // [GET] / news
+    index(req, res) {
+        res.render('home')
+    }
+
+    //[GET] /news/:slug
+    // show(req, res) {
+    //     res.send('new detail')
+    // }
+}
+
+module.exports = new SiteController;
---
+++ src/routes/site.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const SiteController = require('../app/controllers/SiteController')
+
+
+router.use('/', SiteController.index)
+
+
+
+module.exports = router
